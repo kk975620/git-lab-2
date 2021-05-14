d52e2b7c53c65b5e6850f050b164de733e428980
Keerthi Kummari <kk975620@ohio.edu>
2021-05-14 19:08:53 -0400

    First Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Keerthi Kummari
+ *        Date: 05/14/2021
+ * Description: Hello world program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
